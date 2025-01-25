8b9abb07d9429105e4e40dd84ef67da2d7b78036
Tommy <tommyboy4050@outlook.com>
2025-01-25 18:55:59 +0000

    Unit Tests setup

---
+++ Infrastructure.Tests/Repositories/VenueRepositoryTests.cs
@@ -0,0 +1,72 @@
+﻿using Core.Entities;
+using Core.Parameters;
+using Infrastructure.Data.Identity;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Tests.Repositories
+{
+    [TestFixture]
+    public class VenueRepositoryTests : IDisposable
+    {
+        private ApplicationDbContext context;
+        private VenueRepository venueRepository;
+
+        [SetUp]
+        public void Constructor()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Concertable")
+                .Options;
+
+            context = new ApplicationDbContext(options);
+            venueRepository = new VenueRepository(context);
+        }
+
+        [Test]
+        public async Task GetHeadersAsync_ShouldReturnHeaders()
+        { 
+            // Arrange
+            var venueParams = new VenueParams { Sort = "Name" };
+
+            var venueHeaders = new List<Venue> {
+                new Venue { Id = 1, Name = "Test Venue 1", UserId = 1 }, 
+                new Venue { Id = 2, Name = "Test Venue 2", UserId = 2 } 
+            };
+
+            // Populate database
+            context.Venues.AddRange(venueHeaders);
+
+            // Act
+            var result = await venueRepository.GetHeadersAsync(venueParams); 
+            // Assert
+            Assert.NotNull(result); 
+            Assert.AreEqual(venueHeaders.Count(), result.Count()); 
+            Assert.AreEqual(venueHeaders, result);
+        }
+
+        [Test]
+        public async Task GetByUserIdAsync_ShouldReturnVenue()
+        { 
+            // Arrange
+            int userId = 1; 
+            // Act
+            var result = await venueRepository.GetByUserIdAsync(userId); 
+            // Assert
+            Assert.NotNull(result); 
+            Assert.AreEqual(userId, result.UserId); 
+            Assert.AreEqual("Test Venue 1", result.Name);
+        }
+
+        public void Dispose()
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
---
+++ Infrastructure.Tests/Services/VenueServiceTests.cs
@@ -0,0 +1,93 @@
+﻿using Core.Entities;
+using Core.Entities.Identity;
+using Core.Interfaces;
+using Core.Parameters;
+using Infrastructure.Repositories;
+using Infrastructure.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Tests.Services
+{
+    [TestFixture]
+    public class VenueServiceTests
+    {
+        private Mock<IVenueRepository> venueRepositoryMock;
+        private Mock<IAuthService> authServiceMock;
+        private VenueService venueService;
+
+        [SetUp]
+        public void Constructor()
+        {
+            venueRepositoryMock = new Mock<IVenueRepository>();
+            authServiceMock = new Mock<IAuthService>();
+            venueService = new VenueService(venueRepositoryMock.Object, authServiceMock.Object);
+        }
+
+        [Test]
+        public async Task GetVenueHeadersAsync_ShouldReturnHeaders()
+        {
+            // Arrange
+            var venueParams = new VenueParams { Sort = "Name" };
+            IEnumerable<Venue> venueHeaders = new List<Venue>
+            {
+                new Venue { Id = 1, Name = "Venue 1" },
+                new Venue { Id = 2, Name = "Venue 2" }
+            };
+            venueRepositoryMock.Setup(m => m.GetHeadersAsync(venueParams)).ReturnsAsync(venueHeaders);
+
+            // Act
+            var result = await venueService.GetVenueHeadersAsync(venueParams);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(venueHeaders.Count(), result.Count());
+            Assert.AreEqual(venueHeaders, result);
+        }
+
+        [Test]
+        public async Task GetUserVenueAsync_ShouldReturnUsersVenue()
+        { // Arrange
+            var user = new ApplicationUser { Id = 1 };
+            var venue = new Venue { Id = 1, Name = "Venue 1", UserId = user.Id };
+            authServiceMock.Setup(service => service.GetCurrentUser()).ReturnsAsync(user);
+            venueRepositoryMock.Setup(m => m.GetByUserIdAsync(user.Id)).ReturnsAsync(venue);
+            // Act
+            var result = await venueService.GetUserVenueAsync();
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(venue, result);
+        }
+
+        [Test]
+        public async Task CreateVenue_ShouldAddVenue()
+        {
+            // Arrange
+            var user = new ApplicationUser { Id = 1 };
+            var venue = new Venue { Name = "Venue 1" };
+            authServiceMock.Setup(service => service.GetCurrentUser()).ReturnsAsync(user);
+            // Act
+            venueService.CreateVenue(venue);
+            // Assert
+            venueRepositoryMock.Verify(m => m.Add(venue), Times.Once);
+            Assert.AreEqual(user.Id, venue.UserId);
+        }
+
+        public async Task GetVenueDetailsByIdAsync_ShouldReturnVenueDetails()
+        {
+            // Arrange
+            var venueId = 1;
+            var venue = new Venue { Id = 1, Name = "Venue 1" }; 
+            venueRepositoryMock.Setup(m => m.GetByIdAsync(venueId)).ReturnsAsync(venue); 
+            // Act
+            var result = await venueService.GetVenueDetailsByIdAsync(venueId); 
+            // Assert
+            Assert.NotNull(result); 
+            Assert.AreEqual(venue, result); 
+        }
+    }
+}
