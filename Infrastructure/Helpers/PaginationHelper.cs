8fa784b4dec15f3e650ed2c043de1f8aba5d5f06
Tommy <tommyboy4050@outlook.com>
2025-02-07 04:05:35 +0000

    Pagination helper

---
+++ Infrastructure/Helpers/PaginationHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Helpers
+{
+    public static class PaginationHelper
+    {
+        public static IQueryable<T> ApplyPagination<T>(IQueryable<T> query, int page, int pageSize) where T : class
+        {
+            return query.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
