18412f3adf3469063861c6dcb15f4b34c2ef304c
Tommy <tommyboy4050@outlook.com>
2025-02-27 11:32:07 +0000

    Some refinements and bug fixes on the scrollspy sticky nav not always working

--- ClientApp/src/app/components/scrollspy/scrollspy.component.ts
+++ ClientApp/src/app/components/scrollspy/scrollspy.component.ts
@@ -1,40 +1,75 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild, OnDestroy, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { NavItem } from '../../models/nav-item';
 import { ActivatedRoute, Router } from '@angular/router';
+import { fromEvent, Subscription } from 'rxjs';
+import { throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-scrollspy',
   standalone: false,
-  
   templateUrl: './scrollspy.component.html',
   styleUrl: './scrollspy.component.scss'
 })
-export class ScrollspyComponent implements OnInit{
+export class ScrollspyComponent implements AfterViewInit, OnDestroy {
   @Input() navItems: NavItem[] = [];
+  isScrolled = false;
+  activeFragment?: string;
+  navOffsetTop!: number;
+  private sections!: HTMLElement[];
+  private subscriptions: Subscription[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  @ViewChild('nav', { static: true, read: ElementRef }) nav!: ElementRef;
 
-  /*
-  By default, angular doesnt handle fragments if the url 
-  doesnt change, so we have to do it manually
-  */
-  ngOnInit(): void {
-    this.route.fragment.subscribe(fragment => {
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngAfterViewInit(): void {
+    this.sections = this.navItems
+      .map(item => document.getElementById(item.fragment))
+      .filter(section => section !== null)
+
+    this.navOffsetTop = this.nav.nativeElement.offsetTop;
+
+    this.subscriptions.push(this.route.fragment.subscribe(fragment => {
       if (fragment) {
         const element = document.getElementById(fragment);
         if (element) {
-          const navbarOffset = 80; // Adjust for your navbar height
-          window.scrollTo({ 
-            top: element.offsetTop - navbarOffset, 
-            behavior: 'smooth' 
+          const navbarOffset = 80; 
+          window.scrollTo({
+            top: element.offsetTop - navbarOffset,
+            behavior: 'smooth'
           });
         }
       }
-    });
+    }));
+
+    this.subscriptions.push(fromEvent(window, 'scroll')
+      .pipe(throttleTime(100))
+      .subscribe(() => this.handleScroll()));
   }
+
   get queryParams() {
     return this.route.snapshot.queryParams;
   }
 
-  
+  private handleScroll() {
+    this.isScrolled = window.scrollY > this.navOffsetTop;
+
+    let currentSection = this.navItems[0]?.fragment; 
+    const scrollPosition = window.scrollY; 
+
+    for (let section of this.sections) {
+      console.log(section.offsetTop);
+      if (section.offsetTop <= scrollPosition) {
+        currentSection = section.id;
+      }
+    }
+
+    if (this.activeFragment !== currentSection) {
+      this.activeFragment = currentSection;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe()); 
+  }
 }
