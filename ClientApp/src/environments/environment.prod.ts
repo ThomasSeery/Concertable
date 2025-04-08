347f93426243fea28cf19354e34d1fcc6b7d9c90
Tommy <tommyboy4050@outlook.com>
2025-04-08 22:50:30 +0100

    production changes

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  paymentHubUrl: 'https://prod-payment-hub.com',
-  apiUrl: 'https://prod-api.com/api',
-  eventHubUrl: 'https://prod-event-hub.com',  // Add this line
-  stripePublicKey: 'your-production-stripe-public-key'  // Add this line
+  apiUrl: '/api',
+  paymentHubUrl: '/hubs/payment',
+  eventHubUrl: '/hubs/event',
+  stripePublicKey: 'your-production-stripe-key'
 };
