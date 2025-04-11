50e9883dd209252e3b42c1c6aa9c21fa004c23c7
Tommy <tommyboy4050@outlook.com>
2025-04-11 08:00:23 +0100

    added plural to hubs

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
-  paymentHubUrl: '/hubs/payment',
-  eventHubUrl: '/hubs/event',
+  paymentHubUrl: '/hubs/payments',
+  eventHubUrl: '/hubs/events',
   stripePublicKey: 'pk_test_51QqfARQ1mmqr287NAUt5Jsexk7QliOT4zff45kPeCFkaVMkGOdHAotomui7ySCEhnnGluvl8FA5ZiB2QqWkgU3l900E7T6wzTE'
 };
