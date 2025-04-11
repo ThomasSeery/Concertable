749c80dc34e89550d2b36950f8763ea2ed5edf0a
Tommy <tommyboy4050@outlook.com>
2025-04-11 08:20:08 +0100

    removed plural from hubs to hub

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
-  paymentHubUrl: '/hubs/payments',
-  eventHubUrl: '/hubs/events',
+  paymentHubUrl: '/hub/payments',
+  eventHubUrl: '/hub/events',
   stripePublicKey: 'pk_test_51QqfARQ1mmqr287NAUt5Jsexk7QliOT4zff45kPeCFkaVMkGOdHAotomui7ySCEhnnGluvl8FA5ZiB2QqWkgU3l900E7T6wzTE'
 };
