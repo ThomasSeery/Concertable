ba1cf8cf6320b0861016918ec4e9689c443ce680
Tommy <tommyboy4050@outlook.com>
2025-04-11 02:42:28 +0100

    dev controller

---
+++ Web/Controllers/DevController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DevController : ControllerBase
+    {
+        [HttpGet("stripe-key")]
+        public IActionResult GetStripeKey()
+        {
+            var rawEnv = Environment.GetEnvironmentVariable("Stripe__SecretKey");
+            var configKey = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build()["Stripe:SecretKey"];
+
+            return Ok(new
+            {
+                FromEnv = rawEnv?.Substring(0, 8) ?? "NULL",
+                FromConfig = configKey?.Substring(0, 8) ?? "NULL"
+            });
+        }
+    }
+}
