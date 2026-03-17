992950e5d7e4ef7054007be172836e049fed14fd
ThomasSeery <tommyboy4050@outlook.com>
2026-03-17 22:09:04 +0000

    axios interceptors

--- app/src/lib/axios.ts
+++ app/src/lib/axios.ts
@@ -1,8 +1,35 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { useAuthStore } from "@/store/useAuthStore";
 
 const api = axios.create({
   baseURL: "http://localhost:5161/api",
   withCredentials: true,
 });
+
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().accessToken;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  async (error: unknown) => {
+    if (
+      error instanceof AxiosError &&
+      error.response?.status === 401 &&
+      error.config?.url !== "/auth/refresh"
+    ) {
+      await useAuthStore.getState().refresh();
+      const token = useAuthStore.getState().accessToken;
+      if (error.config && token) {
+        error.config.headers.Authorization = `Bearer ${token}`;
+        return api(error.config);
+      }
+    }
+
+    return Promise.reject(error instanceof Error ? error : new Error(String(error)));
+  }
+);
 
 export default api;
