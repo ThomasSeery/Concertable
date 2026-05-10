ce89b8d54b98d81c21a412c9bc08968228f4ceff
Tommy Seery <Tommy.Seery@infonetica.net>
2026-05-10 20:34:18 +0100

    run headless script

---
+++ api/Tests/Concertable.E2ETests/Concertable.E2ETests.Ui/run-ui-tests.ps1
@@ -0,0 +1,7 @@
+param(
+    [switch]$Headed
+)
+
+$env:HEADLESS = if ($Headed) { "false" } else { "true" }
+
+dotnet test $PSScriptRoot/Concertable.E2ETests.Ui.csproj --logger "console;verbosity=normal"
